fa05e1e92f40bb2443268fec0dda61d10cfb6b88
Kévin fixe <kevinrouland.kr@gmail.com>
2019-05-11 20:33:23 +0200

    First integrate Google API

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import ApiCalendar from 'react-google-calendar-api'
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  handleItemClick = (event, name) => {
+    if (name === 'sign-in') {
+      ApiCalendar.handleAuthClick();
+    } else if (name === 'sign-out') {
+      ApiCalendar.handleSignoutClick();
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello Lucky</Text>
+
+        <Button title='sign-in' onPress={(e) => this.handleItemClick(e, 'sign-in')} />
+        
+        <Button title='sign-out' onPress={(e) => this.handleItemClick(e, 'sign-out')}  />
       </View>
-    );
+      
+        
+    )
   }
 }
 
